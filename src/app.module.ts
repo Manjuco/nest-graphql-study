104da2cf5146a90e16522e1b9f74bfb35a5a9966
Manjuco <omanamanju1997@gmail.com>
2026-02-16 22:57:27 +0530

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { HelloResolver } from './hello/hello.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [HelloResolver],
+})
+export class AppModule {}
